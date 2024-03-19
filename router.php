618d91d5b54bd59f927d8727fa56f0f98a3b9302
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2024-03-19 09:40:33 -0300

    organization, refactorin resource guideline

--- router.php
+++ router.php
@@ -7,7 +7,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require "controllers/{$routes[$uri]}.php";
+        require "$routes[$uri]";
     } else {
         abort(404);
     }
--- routes.php
+++ routes.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    "/" => "index",
-    "/about" => "about",
-    '/notes' => 'notes',
-    '/note' => 'note',
-    '/notes/create' => 'note-create',
-    "/contact" => "contact",
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/note' => 'controllers/notes/show.php',
+    '/notes/create' => 'controllers/notes/create.php',
+    '/contact' => 'controllers/contact.php',
 ];
